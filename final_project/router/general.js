1e2e757f86b3f06a0860ea4cc51570649cd80abf
mohamadmasmas <masmasmohamad@outlook.com>
2024-07-14 20:13:38 +0300

    third task

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,13 +19,30 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const book = req.params.isbn
+  if(books[book]){
+    res.send(books[book])
+  }else{
+    return res.status(400).json({message: "Book doesn't exist"});
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let bookKeys = Object.keys(books);
+  let authorBooks = []
+  let selectedAuthor = bookKeys.map((key) => {
+    if (books[key].author == author){
+      authorBooks.push(books[key])
+    }
+  })
+  if(selectedAuthor.length > 0){
+    res.send(authorBooks)
+  }else{
+    return res.status(400).json({message: "Author doesn't exist"});
+  }
 });
 
 // Get all books based on title
